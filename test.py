5787ec4eacdd81c59fcb589f09de9793861121dd
guynavon <53151038+guynavon@users.noreply.github.com>
2019-08-28 10:27:44 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path='C:\\Users\\Admin\\Downloads\\Pyhton downloads\\chromedriver.exe')
+driver.get("http://192.168.99.100:5000/")
